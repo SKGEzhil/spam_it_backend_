32698ea877eba7947a0d2e7a0a8460373cf849c3
SKGEzhil <skgezhil2005@gmail.com>
2023-12-12 17:13:52 +0530

    Added notification, fixed image upload issue

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import boto3, botocore
+import multiprocessing
 import certifi
 from botocore.config import Config
 from flask import Flask, request, jsonify
@@ -12,34 +13,108 @@ from bson import json_util
 from flask_cors import CORS
 import os
 import config
+import firebase_admin
+from firebase_admin import credentials, messaging
+from bson.json_util import dumps
+import threading
 
+
+# Firebase admin sdk
+cred = credentials.Certificate("key.json")
+firebase_admin.initialize_app(cred)
+
+
+# MongoDB connection
 uri = f"mongodb+srv://{config.mongodb_username}:{config.mongodb_password}@{config.mongodb_database}.ot7o9de.mongodb.net/?retryWrites=true&w=majority"
-
-
 client = MongoClient(uri, tlsCAFile=certifi.where())
-
-
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
-
 db = client.sample
 
+# Flask app
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
-
+# S3 connection
 s3 = boto3.client('s3',
                   aws_access_key_id=f'{config.aws_access_key_id}',
                   aws_secret_access_key= f'{config.aws_secret_access_key}',
                   config=Config(signature_version='s3v4')
                   )
-
 BUCKET_NAME='losties'
-
 base_dir = os.getcwd()
+
+# send fcm notification
+@app.route('/send_notification', methods=['GET'])
+def sendNotification(name, body):
+    # The topic name can be optionally prefixed with "/topics/".
+    topic = 'topic'
+
+    # See documentation on defining a message payload.
+    message = messaging.Message(
+        data={
+            'name': name,
+            'body': body,
+        },
+        topic=topic,
+    )
+
+    # Send a message to the devices subscribed to the provided topic.
+    response = messaging.send(message)
+    # Response is a message ID string.
+    print('Successfully sent message:', response)
+    return 'success'
+
+@app.route('/token_auth', methods=['POST'])
+def tokenAuth():
+    json = request.json
+    token = json['token']
+    data = token_encryption.decode(token)
+    roll_no = data['roll_no']
+    password = data['password']
+    user = db.users.find_one({'roll_no': roll_no})
+    if user is None:
+        print('User not found')
+        return 'no_user'
+    else:
+        pwd_hash = user.get('password', '')
+        if password == pwd_hash:
+            print(id)
+            token = token_encryption.encode(json)
+            print(token)
+            data = token_encryption.decode(token)
+            print(data['roll_no'])
+            return 'Success'
+        else:
+            print('incorrect')
+            return 'incorrect'
+
+# generate token
+@app.route('/generate_token', methods=['POST'])
+def generateToken():
+    json = request.json
+    roll_no = json['roll_no']
+    password = json['password']
+    user = db.users.find_one({'roll_no': roll_no})
+    if user is None:
+        print('User not found')
+        return 'no_user'
+    else:
+        pwd_hash = user.get('password', '')
+        if sha256_crypt.verify(password, pwd_hash):
+            print(id)
+            token = token_encryption.encode(json)
+            print(token)
+            data = token_encryption.decode(token)
+            print(data['roll_no'])
+            return token
+        else:
+            print('incorrect')
+            return 'incorrect'
+    return 'login Successfull'
 
 # get username
 @app.route('/get_username', methods=['POST'])
@@ -87,6 +162,8 @@ def upload():
 
     download_link = f"https://{BUCKET_NAME}.s3.amazonaws.com/{filename}"
     print(download_link)
+    os.remove(filename)
+
     return download_link
 
 @app.route('/')
@@ -145,6 +222,9 @@ def createPost():
     image = json['image']
     name = db.users.find_one({'roll_no': roll_no})['name']
     db.posts.insert_one({'roll_no': roll_no, 'name': name, 'subject': subject, 'content': content, 'image': image, 'replies': [], 'date':today.strftime("%d/%m/%Y")})
+
+    sendNotification(name, subject)
+
     return 'success'
 
 @app.route('/get_posts', methods=['GET'])
@@ -164,4 +244,4 @@ def reply():
 
 
 if __name__ == '__main__':
-    app.run(degug=True)
+    app.run(debug=True)
---
+++ test.py
@@ -0,0 +1,16 @@
+import certifi
+import pymongo
+from pymongo import MongoClient
+
+import config
+
+uri = f"mongodb+srv://{config.mongodb_username}:{config.mongodb_password}@{config.mongodb_database}.ot7o9de.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(uri, tlsCAFile=certifi.where())
+db = client.sample
+
+from bson.json_util import dumps
+
+change_stream = db.posts.watch()
+for change in change_stream:
+    print(dumps(change))
+    print('') # for readability only
