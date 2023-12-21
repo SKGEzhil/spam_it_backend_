def4640ba8234a3bc03a127a48747617362db773
SKGEzhil <skgezhil2005@gmail.com>
2023-12-21 12:52:17 +0530

    testing post tag feature (#3)

--- app.py
+++ app.py
@@ -271,7 +271,7 @@ def createPost():
         tagList.append(tag)
 
     name = db.users.find_one({'roll_no': roll_no})['name']
-    db.posts.insert_one({'roll_no': roll_no, 'name': name, 'subject': subject, 'content': content, 'image': image, 'tags': tags, 'date':today.strftime("%d %b")})
+    db.posts.insert_one({'roll_no': roll_no, 'name': name, 'subject': subject, 'content': content, 'image': image, 'tags': tagList, 'date':today.strftime("%d %b")})
 
     sendNotification(name, subject)
 
