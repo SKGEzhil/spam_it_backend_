07309f5097e7d2978540ee6e64e5207948d8b84e
SKGEzhil <skgezhil2005@gmail.com>
2023-12-22 13:40:59 +0530

    adding date to replies db

--- app.py
+++ app.py
@@ -157,12 +157,15 @@ def getUsername():
 
 @app.route('/add_reply', methods=['POST'])
 def addReply():
+
+    today = date.today()
+
     json = request.json
     roll_no = json['roll_no']
     reply = json['reply']
     post_id = json['post_id']
     name = db.users.find_one({'roll_no': roll_no})['name']
-    db.replies.insert_one({'roll_no': roll_no, 'name': name,'reply': reply, 'post_id': post_id})
+    db.replies.insert_one({'roll_no': roll_no, 'name': name,'reply': reply, 'post_id': post_id, 'date':today.strftime("%d %b")})
 
     roll_no_creator = db.posts.find_one({'_id': ObjectId(post_id)})['roll_no']
     name_creator = db.users.find_one({'roll_no': roll_no_creator})['name']
