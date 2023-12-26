1919ba93dd84f43be7a171e242dc4b91db33dfab
SKGEzhil <skgezhil2005@gmail.com>
2023-12-26 18:33:56 +0530

    modified datetime format in replies

--- app.py
+++ app.py
@@ -219,7 +219,7 @@ def addReply():
     reply = json['reply']
     post_id = json['post_id']
     name = db.users.find_one({'roll_no': roll_no})['name']
-    db.replies.insert_one({'roll_no': roll_no, 'name': name,'reply': reply, 'post_id': post_id, 'date':today.strftime("%d %b")})
+    db.replies.insert_one({'roll_no': roll_no, 'name': name,'reply': reply, 'post_id': post_id, 'date':today.strftime("%I:%M %d %b")})
 
     roll_no_creator = db.posts.find_one({'_id': ObjectId(post_id)})['roll_no']
     name_creator = db.users.find_one({'roll_no': roll_no_creator})['name']
